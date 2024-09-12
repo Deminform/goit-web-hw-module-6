046ae0767107277f45a408832b2de9c2ae38ae30
Deminform <deminform@gmail.com>
2024-09-12 19:44:28 +0200

    0.1.11

---
+++ query_9.sql
@@ -0,0 +1,6 @@
+SELECT subjects.subject_name
+FROM scores
+            JOIN students ON scores.student_id = students.id
+            JOIN subjects ON subjects.id = scores.subject_id
+WHERE student_name = 'Neil Drake'
+GROUP BY subjects.subject_name
