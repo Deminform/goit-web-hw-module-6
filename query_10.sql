55d0af971628cc292c8d3fcf9ea5a722eba1d139
Deminform <deminform@gmail.com>
2024-09-13 00:54:44 +0200

    0.1.13

--- query_10.sql
+++ query_10.sql
@@ -1,8 +1,9 @@
 -- Teachers name: Jonathan Frank / Sara Ewing / Jason Herrera / Michelle Hale DDS / Crystal Peck
 SELECT subjects.subject_name
 FROM subjects
-JOIN scores ON scores.subject_id = subjects.id
-JOIN students ON scores.student_id = students.id
-JOIN teachers ON teachers.id = subjects.teacher_id
-WHERE students.student_name = 'Reginald Marshall' AND teachers.teacher_name = 'Jason Herrera'
+         JOIN scores ON scores.subject_id = subjects.id
+         JOIN students ON scores.student_id = students.id
+         JOIN teachers ON teachers.id = subjects.teacher_id
+WHERE students.student_name = 'Reginald Marshall'
+  AND teachers.teacher_name = 'Jason Herrera'
 GROUP BY subjects.subject_name
